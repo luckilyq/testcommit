92ce8b407b5a62820666b7095f8d53e03160f233
luckilyq <1871336898@qq.com>
2021-09-27 11:46:59 +0800

    feat(Scomplie):add IntegerEquals.Java
    add IntegerEquals.java

---
+++ StringEquals.java
@@ -0,0 +1,24 @@
+package top.luckily.java.basic.datatype;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class StringEquals {
+    public static void main(String[] args) {
+        String a = "1";
+        String b = "1";
+        log.info("a == b ? {}",a == b);
+
+        String c = new String("2");
+        String d = new String("2");
+        log.info("c == d ? {}",c == d);
+
+        String e =new String("3").intern();
+        String f =new String("3").intern();
+        log.info("e == f ? {}",e == f);
+
+        String g = new String("4");
+        String h = new String("4");
+        log.info("g == h ? {}", g.equals(h));
+    }
+}
