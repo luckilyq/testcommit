213b6c1ad04112d60fb34e61a24501b1cec7b1b5
luckilyq <1871336898@qq.com>
2021-09-27 11:36:15 +0800

    docs(tutorial):add README.md file
    add add README.md file
    - 1.1
    - 1.2
    - 1.3
    
    Breaks tutorial.quickstart,help developers

---
+++ IntergerEquals.java
@@ -0,0 +1,29 @@
+package top.luckily.java.basic.datatype;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class IntergerEquals {
+    public static void main(String[] args) {
+        Integer a = 127;
+        Integer b = 127;
+        log.info("a == b ? {}", a == b );
+
+        Integer c = 128;
+        Integer d = 128;
+        log.info("c == d ? {}", c == d );
+
+        Integer e = 127;
+        Integer f = new Integer(127);
+        log.info("e == f ? {}", e == f );
+
+        Integer g = new Integer(127);
+        Integer h = new Integer(127);
+        log.info("g == h ? {}", g == h);
+
+        Integer i = 128;
+        int j = 128;
+        log.info("i == j ? {}", i == j);
+    }
+}
